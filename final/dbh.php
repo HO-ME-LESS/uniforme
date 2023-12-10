e2dbc8725dbfce6897d5e4275d71c1321dc3cab8
HO-ME-LESS <153457033+HO-ME-LESS@users.noreply.github.com>
2023-12-11 03:13:08 +0800

    Add files via upload

---
+++ final/dbh.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "localhost";
+$username = "id21651163_unifoo";
+$password = "Kutabare1!";
+$dbname = "id21651163_unifoo";
+
+$conn = mysqli_connect($server, $username,$eMail $password, $dbname);
+
+?>
---
+++ final/includes/footer.php
@@ -0,0 +1,39 @@
+<!--Footer-->
+<footer class="mt-5 py-5">
+        <div class="row container mx-auto pt-5">
+          <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+            <img class="logo" src="images/psau.jpg"/>
+           
+          </div>
+
+          <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+            <h5 class="pb-2">Contact Us</h5>
+            <div>
+              <h6 class="text-uppercase">Address</h6>
+              <p>PSAU MAGALANG,PAMPANGA</p>
+            </div>
+            <div>
+              <h6 class="text-uppercase">Phone</h6>
+              <p>0977-140-6964</p>
+            </div>
+            <div>
+              <h6 class="text-uppercase">Email</h6>
+              <p>Uniforme@gmail.com</p>
+            </div>
+        
+
+          <div class="copyright mt-5">
+            <div class="row container mx-auto">
+              <div class="col-lg-3 col-md-5 col-sm-12 mb-4">
+                
+              </div>
+              <div class="col-lg-3 col-md-5 col-sm-12 mb-4 text-nowrap mb-2">
+                <p>UNIFORME All Right Reserved</p>
+            </div>
+
+          
+
+        </div>
+
+        </div>
+      </footer>
